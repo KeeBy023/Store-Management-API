b11fb35164a6b26659cf90ee62f43489611d65e1
Danut Radu <d_dany_radu@yahoo.com>
2025-08-27 18:55:33 +0300

    Create Product entity with JPA annotations

---
+++ src/main/java/com/danutradu/storemanagementapi/entity/Product.java
@@ -0,0 +1,30 @@
+package com.danutradu.storemanagementapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
