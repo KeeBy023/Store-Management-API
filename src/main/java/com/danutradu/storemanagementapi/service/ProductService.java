045dbc427585e16107102785f20f609fed4b57ef
Danut Radu <d_dany_radu@yahoo.com>
2025-08-28 20:20:03 +0300

    Update product service to send event on price change

--- src/main/java/com/danutradu/storemanagementapi/service/ProductService.java
+++ src/main/java/com/danutradu/storemanagementapi/service/ProductService.java
@@ -2,15 +2,19 @@ package com.danutradu.storemanagementapi.service;
 
 import com.danutradu.storemanagementapi.dto.ProductDto;
 import com.danutradu.storemanagementapi.entity.Product;
+import com.danutradu.storemanagementapi.event.ProductPriceChangedEvent;
 import com.danutradu.storemanagementapi.exception.ProductNotFoundException;
 import com.danutradu.storemanagementapi.mapper.ProductMapper;
 import com.danutradu.storemanagementapi.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +23,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final ApplicationEventPublisher eventPublisher;
 
     public Product findProductById(Long id) {
         log.info("Searching product by id: {}", id);
@@ -41,11 +46,21 @@ public class ProductService {
         return productRepository.save(product);
     }
 
+    @Transactional
     public Product updateProduct(Long id, ProductDto updates) {
         log.info("Updating product with id: {}", id);
         var product = findProductById(id);
+        var oldPrice = product.getPrice();
+
         productMapper.updateEntityFromDto(updates, product);
-        return productRepository.save(product);
+        var savedProduct = productRepository.save(product);
+
+        if (updates.getPrice() != null && !oldPrice.equals(updates.getPrice())) {
+            var currentUser = SecurityContextHolder.getContext().getAuthentication().getName();
+            eventPublisher.publishEvent(new ProductPriceChangedEvent(savedProduct, oldPrice, updates.getPrice(), currentUser));
+        }
+
+        return savedProduct;
     }
 
     public void deleteProduct(Long id) {
